03d6a06ee5ee32582b7085d87c64b0d824f7b4cb
Nancy Chaudhry <nancych14@gmail.com>
2024-04-18 17:16:21 +0500

    First commit

---
+++ chatbot.py
@@ -0,0 +1,52 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+# accepts a preferred model and a list of messages
+# makes chat completions API call
+# returns the response message content
+def get_api_chat_response_message(model, messages):
+    # make the API call
+    api_response = client.chat.completions.create(
+        model = model,
+        messages = messages
+    )
+
+    # extract the response text
+    response_content = api_response.choices[0].message.content
+
+    # return the response text
+    return response_content
+
+model = "gpt-3.5-turbo"
+
+chat_history = []
+
+if len(chat_history) == 0:
+    greeting = "Hi, You can type 'exit' to end the conversation.  What's your name?"
+    print(greeting)
+    chat_history.append({
+        "role": "assistant",
+	    "content": greeting
+    })
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        break
+
+    chat_history.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    response = get_api_chat_response_message(model, chat_history)
+
+    print("Chatbot: ", response)
+
+    chat_history.append({
+	    "role": "assistant",
+	    "content": response
+    })
+
+print("See you later!")
